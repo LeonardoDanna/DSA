3fc05984bdc7294fd5c762dc603892fecef2f243
Leonardo Danna <leonardosouzadanna@gmail.com>
2025-02-05 17:40:48 -0300

    update

--- bubble_sort.py
+++ bubble_sort.py
@@ -3,4 +3,9 @@ def bubble_sort(arr):
     for i in range(n): # Loop externo para controlar o número de iterações
         for j in range(0, n - i - 1): # Loop interno para comparar e trocar elementos
             if arr[j] > arr[j + 1]: # Verifica se o elemento atual é maior que o próximo
+                is_sorted = True # Define que a lista ainda não está ordenada
                 arr[j], arr[j + 1] = arr[j + 1], arr[j] # Troca os elementos
+        if is_sorted: # Se a lista já estiver ordenada, interrompe o loop
+            return # Retorna a lista ordenada
+        
+bubble_sort([1, 3, 2, 5, 4])
